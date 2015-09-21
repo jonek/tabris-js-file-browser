0a6b459c87430d1f1ca0c09b22ba12613ea022ca
Johannes Eickhold <jeick@eclipsesource.com>
2015-09-22 00:06:17 +0200

    Fix layout, add up button

--- www/app.js
+++ www/app.js
@@ -8,33 +8,45 @@ tabris.create("Page", {
 
 function createExample(page) {
   var rootDirMappings = [
-    {name: ".applicationDirectory", value: cordova.file.applicationDirectory},
-    {name: ".applicationStorageDirectory", value: cordova.file.applicationStorageDirectory},
-    {name: ".externalApplicationStorageDirectory", value: cordova.file.externalApplicationStorageDirectory},
-    {name: ".externalRootDirectory", value: cordova.file.externalRootDirectory}
+    {name: "cordova.file.applicationDirectory", value: cordova.file.applicationDirectory},
+    {name: "cordova.file.applicationStorageDirectory", value: cordova.file.applicationStorageDirectory},
+    {name: "cordova.file.externalApplicationStorageDirectory", value: cordova.file.externalApplicationStorageDirectory},
+    {name: "cordova.file.externalRootDirectory", value: cordova.file.externalRootDirectory}
   ];
 
   var rootDirPickerLabel = tabris.create("TextView", {
-    layoutData: {left: 0, top: 0, right: 0},
-    text: "Cordova File Plugin \"cordova.file...\""
+    layoutData: {left: 16, top: 8, right: 16},
+    text: "Cordova Filesystem Entry Point",
+    textColor: "#009688"
   }).appendTo(page);
 
   var rootDirPicker = tabris.create("Picker", {
-    layoutData: {left: 0, top: [rootDirPickerLabel, 0], right: 0},
+    layoutData: {left: 16, top: [rootDirPickerLabel, 0], height: 48, right: 16},
     items: rootDirMappings.map(function(item) {return item.name;}),
     selectionIndex: 0
   }).appendTo(page);
 
+  var urlBar = tabris.create("Composite", {
+    layoutData: {left: 0, right: 0, top: [rootDirPicker, 0], height: 48},
+  }).appendTo(page);
+
   var currentDirectoryView = tabris.create("TextInput", {
-    layoutData: {left: 0, top: [rootDirPicker, 0], right: 0},
+    layoutData: {left: 16, top: 0, right: [0, 80]},
+    font: "16px",
     editable: false,
     autoCorrect: false,
     text: currentDirectory
-  }).appendTo(page);
+  }).appendTo(urlBar);
+
+  var upButton = tabris.create("Button", {
+    layoutData: {left: [currentDirectoryView, 16], right: 16, top: 0},
+    text: "Up",
+    enabled: false
+  }).appendTo(urlBar);
 
   var scrollView = tabris.create("ScrollView", {
     direction: "vertical",
-    layoutData: {left: 0, top: [currentDirectoryView, 0], right: 0, bottom: 0}
+    layoutData: {left: 0, top: [urlBar, 16], right: 0, bottom: 0}
   }).appendTo(page);
 
   var success = function(param) {
@@ -43,13 +55,16 @@ function createExample(page) {
     console.log("directoryReader:\n" + JSON.stringify(directoryReader));
     directoryReader.readEntries(function(result) {
       result.forEach(function (entry) {
+        var row = tabris.create("Composite", {
+          layoutData: {left: 0, right: 0, top: ["prev()", 1], height: 72},
+          background: "#ddd"
+        }).appendTo(scrollView);
         var dirEntryView = tabris.create("TextView", {
-          layoutData: {left: 3, right: 3, top: ["prev()", 3]},
+          layoutData: {left: 16, right: 16, centerY: 0},
           font: "20px",
-          background: "#ddd",
           text: entry.fullPath,
           markupEnabled: true
-        }).appendTo(scrollView);
+        }).appendTo(row);
         if(entry.isDirectory) {
           dirEntryView.set("font", "bold 20px");
           dirEntryView.on("tap", function() {
@@ -70,8 +85,8 @@ function createExample(page) {
     window.resolveLocalFileSystemURL(currentDirectory, success, error);
   }
 
-  rootDirPicker.on("change:selectionIndex", function(picker, index) {
-    changeDir(rootDirMappings[index].value);
+  rootDirPicker.on("select", function(picker, selection, options) {
+    changeDir(rootDirMappings[options.index].value);
   });
 
 }
